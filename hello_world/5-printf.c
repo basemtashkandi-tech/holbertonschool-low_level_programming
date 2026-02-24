c7f5e94461fbdeff642c6cebd28a75fe8a1dfda4
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-02-24 22:22:43 +0300

    Task 5 att1

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints.
+ *
+ * Description: prints with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
