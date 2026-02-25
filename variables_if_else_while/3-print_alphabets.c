d1c32d381539528473b3eec7c0b00039d826790a
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-02-25 22:15:24 +0300

    Task 3 att1

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints alphabet in lower case then uppercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
