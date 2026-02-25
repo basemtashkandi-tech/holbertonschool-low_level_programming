facec68c892895dc9ec2efe596aeb707b1780114
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-02-25 21:17:03 +0300

    Task 0 att1

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main function
+ *
+ * Description: checks random number if positive, negative or zero.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive");
+	}
+	else if (n < 0)
+	{
+		printf("is negative");
+	}
+	else
+	{
+		printf("is zero");
+	}
+	return (0);
+}
