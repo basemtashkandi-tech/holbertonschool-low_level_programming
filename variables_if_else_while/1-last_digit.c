212cff4c1a8c0f61cef7799d626c42212226f973
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-02-25 21:42:35 +0300

    Task 1 att4

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,12 +12,15 @@
 int main(void)
 {
 	int n;
-	int last_digit = n % 10;
+
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
 
 	printf("Last digit of %d is %d and is ", n, last_digit);
+
 	if (last_digit > 5)
 	{
 		printf("greater than 5\n");
@@ -26,10 +29,9 @@ int main(void)
 	{
 		printf("0\n");
 	}
-	else if (last_digit < 6 && last_digit != 0)
+	else
 	{
 		printf("less than 6 and not 0\n");
 	}
-	else
 	return (0);
 }
