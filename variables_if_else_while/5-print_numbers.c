883f7aa62072f69c911fb5acb4b784f1afb0bdf8
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-02-25 22:52:39 +0300

    Task 5 att1

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
