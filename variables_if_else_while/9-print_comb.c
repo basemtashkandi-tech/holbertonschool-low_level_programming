87ac0bb7ad16ca062306dd594d9a4bf306d24ee7
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-02-25 23:23:21 +0300

    Task 9 att1

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
