c8902776c6b799b613720205530802041001309a
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-02-27 03:30:17 +0300

    Task 7 att1

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: the number
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	if (n < 0)
+	n = -n;
+	_putchar('0' + n);
+	return (n);
+}
