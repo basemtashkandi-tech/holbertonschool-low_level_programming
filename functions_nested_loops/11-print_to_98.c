8eddffea27d4169fad6f4c69bbd165477bca863d
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-02-27 05:50:16 +0300

    Task 11 att1

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print to the number 98
+ * @n: the number to start from
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n == 98)
+			{
+				continue;
+			}
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n == 98)
+			{
+				continue;
+			}
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		printf("98\n");
+	}
+}
