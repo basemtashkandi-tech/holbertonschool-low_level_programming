c40ec8fd8cba8390b39416af9d358829e527da2e
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-02-27 03:15:27 +0300

    Task 6 att1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ *
+ * Return: absolute value
+ * @n: the number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	return (n);
+}
