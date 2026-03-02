17e2f9316dfcea39c7054ddb930c781f29f1ccef
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-03-02 21:07:51 +0300

    Task 1 att1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if digit
+ * @c: the character to check
+ *
+ * Return: (1) if digit and (0) otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
