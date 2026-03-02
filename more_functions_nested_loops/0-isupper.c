8de6a5b3bc8f7573bc4c92ec543f2ecf587ca054
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-03-02 21:03:27 +0300

    Task 0 att1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase checker
+ * @c: the character to check
+ *
+ * Return: (1) if upper and (0) otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
