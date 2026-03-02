009bc68748b232e6e3e0ab89f415ee8fbcaaee32
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-03-02 23:40:15 +0300

    Task 7 att1

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal line
+ * @n: size
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
