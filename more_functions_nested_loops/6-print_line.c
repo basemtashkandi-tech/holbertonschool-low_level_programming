081d17fd9a80fb7f1ef7c98156125b7147989d2e
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-03-02 22:54:52 +0300

    Task 6 att1

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines
+ * @n: the number of lines to print
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
