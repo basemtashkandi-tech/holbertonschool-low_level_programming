8224be490b0a037c81649be9b8b37207d7949df1
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-03-09 23:20:58 +0300

    Rename

---
+++ pointers_arrays_strings_2/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: first occurrence in the string
+ * @needle: finds the first occurrence of the substring needle
+ *
+ * Return: a pointer to the beginning of the located substring.
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	if (!*needle)
+		return (haystack);
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return (NULL);
+}
