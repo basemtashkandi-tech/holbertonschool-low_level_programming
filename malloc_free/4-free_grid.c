98267d78d970f60f5deba3307cf019fb3a43299d
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-04-02 17:55:52 +0300

    Task 4 att1

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - description
+ * @grid: description
+ * @height: description
+ * Return: description
+ */
+void free_grid(int **grid, int height)
+{
+	int size = sizeof(grid) / sizeof(int);
+}
