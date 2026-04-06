1f1f591532f152fabd5d9bda9c1e6528061a5fd7
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-04-06 17:38:23 +0300

    Task 2 att1

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - description
+ * @d: description
+ *
+ * Return: description
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: %s\n", "(nil)");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
